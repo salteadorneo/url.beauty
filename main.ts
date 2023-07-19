4ef4758112a2c2eff993ea0631939576327c05bd
Cristian Adán <hola@salteadorneo.dev>
2023-07-20 00:10:08 +0200

    add pages

--- main.ts
+++ main.ts
@@ -1,90 +1,79 @@
-import { ConnInfo, serve } from "https://deno.land/std@0.142.0/http/server.ts";
+import { ConnInfo, serve } from "https://deno.land/std@0.142.0/http/server.ts"
+
+import { create, home } from "./pages.ts"
+
+const ORIGIN = "https://url.beauty"
+Deno.env.set("ORIGIN", ORIGIN)
 
 serve(async (_req: Request, connInfo: ConnInfo): Promise<Response> => {
-  const { pathname, searchParams } = new URL(_req.url);
-  const query = pathname.substring(1);
+  const { pathname } = new URL(_req.url)
+  const query = pathname.substring(1)
 
-  const stats = searchParams.get("stats");
-
-  const kv = await Deno.openKv();
-
-  if (query === "all") {
-    const entries = kv.list({ prefix: ["links"] });
-
-    const result = [];
-    for await (const entry of entries) {
-      result.push(entry);
-    }
-
-    return new Response(JSON.stringify(result), {
+  if (!query) {
+    return new Response(home(), {
       status: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-    });
+      headers: { "Content-Type": "text/html" },
+    })
   }
+
+  const kv = await Deno.openKv()
 
   if (query.startsWith("http")) {
-    const hash = await hashStr(query);
+    const hash = await hashStr(query)
 
-    const onlyNums = hash.replace(/\D/g, "");
+    const onlyNums = hash.replace(/\D/g, "")
 
-    const id = idToShortURL(onlyNums);
+    const id = idToShortURL(onlyNums)
 
-    let res = await kv.get(["links", id]);
+    let res = await kv.get(["links", id])
+
     if (!res || !res.value) {
-      await kv.set(["links", id], { path: query, count: 0, requests: [] });
-      res = await kv.get(["links", id]);
+      await kv.set(["links", id], { path: query, count: 0, requests: [] })
+      res = await kv.get(["links", id])
     }
 
-    return new Response(JSON.stringify(res), { status: 200 });
-  } else {
-    const res = await kv.get(["links", query]);
-
-    if (stats) {
-      return new Response(JSON.stringify(res), {
-        status: 200,
-        headers: { "Access-Control-Allow-Origin": "*" },
-      });
-    }
-
-    const { value } = res;
-
-    if (!value) {
-      return Response.redirect("https://web.url.beauty", 307);
-    }
-
-    const { path, count = 0, requests = [] } = value;
-
-    requests.push({
-      ip: connInfo.remoteAddr?.hostname,
-      userAgent: _req.headers.get("user-agent"),
-      time: new Date().toUTCString(),
-    });
-
-    kv.set(["links", query], { path, count: count + 1, requests });
-
-    return Response.redirect(path, 307);
+    return new Response(create(res), {
+      status: 200,
+      headers: { "Content-Type": "text/html" },
+    })
   }
-});
+
+  const { value } = await kv.get(["links", query])
+
+  if (!value) {
+    return Response.redirect(ORIGIN, 307)
+  }
+
+  const { path, count = 0, requests = [] } = value
+
+  requests.push({
+    ip: connInfo.remoteAddr?.hostname,
+    userAgent: _req.headers.get("user-agent"),
+    time: new Date().toUTCString(),
+  })
+
+  kv.set(["links", query], { path, count: count + 1, requests })
+
+  return Response.redirect(path, 307)
+})
 
 async function hashStr(message: string) {
-  const data = new TextEncoder().encode(message);
-  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const data = new TextEncoder().encode(message)
+  const hashBuffer = await crypto.subtle.digest("SHA-256", data)
+  const hashArray = Array.from(new Uint8Array(hashBuffer))
   const hashHex = hashArray
     .map((b) => b.toString(16).padStart(2, "0"))
-    .join("");
-  return hashHex;
+    .join("")
+  return hashHex
 }
 
-function idToShortURL(n) {
-  const map = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  const shorturl = [];
+function idToShortURL(n: any) {
+  const map = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  const shorturl = []
   while (n) {
-    shorturl.push(map[n % 62]);
-    n = Math.floor(n / 62);
+    shorturl.push(map[n % 62])
+    n = Math.floor(n / 62)
   }
-  shorturl.reverse();
-  return shorturl.join("").slice(0, 5);
+  shorturl.reverse()
+  return shorturl.join("").slice(0, 5)
 }
