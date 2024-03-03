7aad3658e6c0edd42cf88e272dda9258362d09f5
Cristian Adán <hola@salteadorneo.dev>
2024-03-03 02:23:06 +0100

    Add SEO

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,14 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>URL Beauty</title>
+        <title>
+          URL Beauty | Make beauty any link | URL shortener
+        </title>
+        <meta
+          name="description"
+          content="A free URL shortener fast and easy to use. Beautify your links with URL Beauty."
+        />
+        <link rel="icon" href="/favicon.svg" />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
