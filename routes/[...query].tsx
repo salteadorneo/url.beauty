4c2898fc83976deaeeb14b5969e6f23b016174ef
Cristian Adán <hola@salteadorneo.dev>
2024-03-08 22:05:32 +0100

    Fix responsive + Add charts referrer & ip location

--- routes/[...query].tsx
+++ routes/[...query].tsx
@@ -1,6 +1,7 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { qrcode } from "https://deno.land/x/qrcode@v2.0.0/mod.ts";
 import { Chart } from "$fresh_charts/mod.ts";
+import { getIPLocation } from "https://deno.land/x/ip_location/mod.ts";
 
 import { hashStr, idToShortURL } from "../utils.ts";
 import Footer from "../components/Footer.tsx";
@@ -9,6 +10,11 @@ import CopyToClipboard from "../islands/CopyToClipboard.tsx";
 
 type Request = {
   ip: string;
+  city: string;
+  country: string;
+  country_name: string;
+  latitude: number;
+  longitude: number;
   time: Date;
   userAgent: string;
   referer: string;
@@ -48,11 +54,17 @@ export const handler: Handlers = {
 
     const { path, count = 0, requests = [] } = value;
 
+    const location = await getIPLocation();
     const userAgent = _req.headers.get("user-agent") || "";
     const referer = _req.headers.get("referer") || "";
 
     requests.push({
-      ip: ctx.remoteAddr?.hostname,
+      ip: location?.ip ?? ctx.remoteAddr?.hostname,
+      city: location?.city ?? "",
+      country: location?.country ?? "",
+      country_name: location?.country_name ?? "",
+      latitude: location?.latitude ?? 0,
+      longitude: location?.longitude ?? 0,
       userAgent,
       referer,
       time: new Date().toUTCString(),
@@ -94,6 +106,7 @@ export default function Page(props: PageProps) {
     date.setDate(date.getDate() - i);
     return date.toISOString().split("T")[0];
   }).sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
+
   // get count of requests for each label
   const data = labels.map((label) => {
     const date = new Date(label);
@@ -108,10 +121,25 @@ export default function Page(props: PageProps) {
     return { label, count };
   });
 
+  // get referrers grouped by domain
+  const referrers = requests.reduce((acc, req) => {
+    if (!req.referer) return acc;
+    const domain = new URL(req.referer).hostname;
+    acc[domain] = (acc[domain] || 0) + 1;
+    return acc;
+  }, {} as Record<string, number>);
+
+  // get country name and count of requests
+  const countries = requests.reduce((acc, req) => {
+    if (!req.country_name) return acc;
+    acc[req.country_name] = (acc[req.country_name] || 0) + 1;
+    return acc;
+  }, {} as Record<string, number>);
+
   return (
     <main class="max-w-screen-lg mx-auto px-4 text-zinc-800">
       <Header />
-      <section class="flex items-center justify-between gap-2 p-4 border border-zinc-200 rounded bg-zinc-100">
+      <section class="flex flex-col sm:flex-row items-center justify-between flex-wrap gap-2 p-4 border border-zinc-200 rounded bg-zinc-100">
         <div class="flex flex-col">
           <div class="flex items-center gap-1">
             <a href={`${ORIGIN}/${hash}`} target="_blank" class="font-semibold">
@@ -124,37 +152,106 @@ export default function Page(props: PageProps) {
           </p>
         </div>
 
-        <img src={qr} alt="QR code" class="border-2 border-zinc-200 rounded" />
-
-        <a
-          href={`${ORIGIN}/${path}`}
-          class="flex items-center gap-1 bg-zinc-200 hover:bg-zinc-300 rounded-full py-0.5 px-2"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            class="w-4 h-4"
-          >
-            <path d="M12 2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1h-1ZM6.5 6a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V6ZM2 9a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V9Z" />
-          </svg>
-          {count} clicks
-        </a>
-      </section>
-      <section class="flex items-center gap-2 p-4 border rounded mt-6">
-        <Chart
-          type="bar"
-          width={950}
-          data={{
-            labels,
-            datasets: [
-              {
-                label: "Clicks",
-                data: data.map((d) => d.count),
-              },
-            ],
-          }}
+        <img
+          src={qr}
+          alt="QR code"
+          class="border-2 border-zinc-200 rounded"
         />
+
+        <section class="flex items-center gap-2">
+          <a
+            href={qr}
+            class="flex items-center gap-1 bg-zinc-200 hover:bg-zinc-300 rounded-full py-0.5 px-2 text-sm"
+            download="qr.gif"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              class="w-4 h-4"
+            >
+              <path d="M10.75 2.75a.75.75 0 0 0-1.5 0v8.614L6.295 8.235a.75.75 0 1 0-1.09 1.03l4.25 4.5a.75.75 0 0 0 1.09 0l4.25-4.5a.75.75 0 0 0-1.09-1.03l-2.955 3.129V2.75Z" />
+              <path d="M3.5 12.75a.75.75 0 0 0-1.5 0v2.5A2.75 2.75 0 0 0 4.75 18h10.5A2.75 2.75 0 0 0 18 15.25v-2.5a.75.75 0 0 0-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5Z" />
+            </svg>
+            Download QR
+          </a>
+          <a
+            href={`${ORIGIN}/${path}#stats`}
+            class="flex items-center gap-1 bg-zinc-200 hover:bg-zinc-300 rounded-full py-0.5 px-2 text-sm"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              class="w-4 h-4"
+            >
+              <path d="M12 2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1h-1ZM6.5 6a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V6ZM2 9a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V9Z" />
+            </svg>
+            Stats
+          </a>
+        </section>
+      </section>
+      <section
+        id="stats"
+        class="flex flex-wrap justify-between gap-2 p-4 border rounded mt-6"
+      >
+        <div class="grid">
+          <p>
+            <strong>Clicks</strong> over the last 7 days
+          </p>
+          <Chart
+            type="bar"
+            width={300}
+            height={300}
+            data={{
+              labels,
+              datasets: [
+                {
+                  label: "Clicks",
+                  data: data.map((d) => d.count),
+                },
+              ],
+            }}
+          />
+        </div>
+        <div class="grid">
+          <p>
+            <strong>Referrers</strong>
+          </p>
+          <Chart
+            type="pie"
+            width={260}
+            height={260}
+            data={{
+              labels: Object.keys(referrers),
+              datasets: [
+                {
+                  label: "Referrers",
+                  data: Object.values(referrers),
+                },
+              ],
+            }}
+          />
+        </div>
+        <div class="grid">
+          <p>
+            <strong>Countries</strong>
+          </p>
+          <Chart
+            type="pie"
+            width={260}
+            height={260}
+            data={{
+              labels: Object.keys(countries),
+              datasets: [
+                {
+                  label: "Countries",
+                  data: Object.values(countries),
+                },
+              ],
+            }}
+          />
+        </div>
       </section>
       <Footer />
     </main>
