09e2c89ca7391fa78431d0ec9123da23208954e9
Cristian Adán <hola@salteadorneo.dev>
2024-03-14 00:03:48 +0100

    Add Open Graph and Twitter Card meta tags for URL Beauty website

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -14,6 +14,32 @@ export default function App({ Component }: PageProps) {
           content="A free URL shortener fast and easy to use. Beautify your links with URL Beauty."
         />
         <link rel="icon" href="/favicon.svg" />
+
+        <meta property="og:url" content="https://url.beauty/" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="URL Beauty | Make beauty any link | URL shortener"
+        />
+        <meta
+          property="og:description"
+          content="A free URL shortener fast and easy to use. Beautify your links with URL Beauty."
+        />
+        <meta property="og:image" content="https://url.beauty/og.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="url.beauty" />
+        <meta property="twitter:url" content="https://url.beauty/" />
+        <meta
+          name="twitter:title"
+          content="URL Beauty | Make beauty any link | URL shortener"
+        />
+        <meta
+          name="twitter:description"
+          content="A free URL shortener fast and easy to use. Beautify your links with URL Beauty."
+        />
+        <meta name="twitter:image" content="https://url.beauty/og.png" />
+
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
