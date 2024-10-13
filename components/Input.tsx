4cf2c228e1fd927b7ba0ca53cec7096b77a50f34
Cristian Adán <hola@salteadorneo.dev>
2024-10-13 21:10:51 +0200

    Refactor Input component to add a fixed width of 64 pixels

--- components/Input.tsx
+++ components/Input.tsx
@@ -4,9 +4,8 @@ export default function Input(props: JSX.HTMLAttributes<HTMLInputElement>) {
   return (
     <input
       {...props}
-      class={`px-3 py-2 bg-white rounded border disabled:(opacity-50 cursor-not-allowed) ${
-        props.class ?? ""
-      }`}
+      class={`px-3 py-2 bg-white rounded border w-64 disabled:(opacity-50 cursor-not-allowed) ${props.class ?? ""
+        }`}
     />
   );
 }
